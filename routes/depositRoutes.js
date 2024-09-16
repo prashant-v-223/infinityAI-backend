2da0e3c01eb43415933ddbc7993a8b5f4d4bb865
Daydreaminfotech94 <daydreaminfotech287@gmail.com>
2024-09-16 14:13:02 +0530

    asd

--- routes/depositRoutes.js
+++ routes/depositRoutes.js
@@ -1,11 +1,11 @@
 // routes/depositRoutes.js
 const express = require('express');
 const { createDeposit, getDeposit, getDepositall, updateDeposit } = require('../controllers/depositController');
-const router = express.Router();
+const depositController = require('../controllers/depositController'); // Ensure this path is correct
 
 router.post('/deposit', createDeposit);
 router.get('/deposit', getDeposit);
 router.get('/alldeposit', getDepositall);
-router.put('/deposits/:id', updateDeposit);
+router.put('/deposits/:id', depositController.updateDeposit);
 
 module.exports = router;
